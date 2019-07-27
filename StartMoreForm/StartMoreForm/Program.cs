88f7e42442fa1aafc55b0c66dfd9947d9d8c786b
jinshuai <jinshuai518@163.com>
2019-07-27 21:51:47 +0800

    同时打开多个窗体

---
+++ StartMoreForm/StartMoreForm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartMoreForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //注销 20190706
+            //Application.Run(new ELFrm());
+            //调用用于显示窗体的类
+            Application.Run(new MultiFrmApplictionStart());    
+        }
+    }
+}
---
+++ StartMoreForm/StartMoreForm/StartFrm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//增加头文件
+using System.Windows.Forms;
+
+
+namespace StartMoreForm
+{
+    class MultiFrmApplictionStart : ApplicationContext
+    {
+        /// <summary>
+        /// 多窗口同时启动类
+        /// <remarks>继承ApplicationContext的原因是Application.Run(ApplicationContext context)参数的需要</remarks>
+        /// <remarks>另一个是关闭同时启动的窗口</remarks>
+        /// </summary>
+        private void onFormClosed(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.Count == 0)
+            {
+                ExitThread();
+            }
+        }
+
+        /// <summary>
+        /// 遍历所有窗体
+        /// </summary>
+        public MultiFrmApplictionStart()
+        {
+            //将要显示的窗体集合
+            var formList = new List<Form>()
+            {
+                new ELFrm(),
+                new WGFrm()
+            };
+
+            foreach (var item in formList)
+            {
+                item.FormClosed += onFormClosed;
+            }
+
+            foreach (var item in formList)
+            {
+                item.Show();
+            }
+        }
+    }
+}
